54b3f89a105331911f516df5e11f66792607b1b5
JohnAthanasopoulos <61366457+John-Athanasopoulos@users.noreply.github.com>
2020-08-01 05:00:01 +0300

    Create Cards.java

---
+++ Classes/Cards.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+
+public class Cards {
+    static ArrayList<String> cards = new ArrayList<String>();
+    static ArrayList<String> remCards = new ArrayList<String>();
+}
